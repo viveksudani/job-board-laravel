2a2c910bf293e12379f2bc1ef3ffb3a2652f9d1d
Vivek Sudani <viveksudani7@gmail.com>
2025-08-07 10:42:11 +0530

    add nav-link component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 
 </head>
-<body>
+<body class="h-full">
 
 <div class="min-h-full">
   <nav class="bg-gray-800">
@@ -21,9 +21,9 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
-              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-              <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+              <x-nav-link href="/" :active="request() -> is('/')">Home</x-nav-link>
+              <x-nav-link href="/about" :active="request() -> is('about')">About</x-nav-link>
+              <x-nav-link href="/contact" :active="request() -> is('contact')" >Contact</x-nav-link>
             </div>
           </div>
         </div>
