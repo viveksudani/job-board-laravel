3cc43baa852cf402f77d019abb3b0e669f15071a
Vivek Sudani <viveksudani7@gmail.com>
2025-08-07 18:38:31 +0530

    added JobFactory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
+
+    use HasFactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
