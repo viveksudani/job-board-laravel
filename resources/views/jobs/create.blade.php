53b5a96079cca3e52573c3d85879ff01f5555558
Vivek Sudani <viveksudani7@gmail.com>
2025-08-10 18:32:43 +0530

    added form validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -15,8 +15,12 @@
           <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="title" type="text" name="title" placeholder="Software Engineer" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="title" type="text" name="title" placeholder="Software Engineer" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
             </div>
+
+            @error('title')
+               <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }} </p>
+            @enderror
           </div>
         </div>
 
@@ -24,12 +28,27 @@
           <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="salary" type="text" name="salary" placeholder="$ 50,000 Per Year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="salary" type="text" name="salary" placeholder="$ 50,000 Per Year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
             </div>
+
+            @error('salary')
+               <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }} </p>
+            @enderror
           </div>
         </div>
 
       </div>
+      <!-- 
+      <div class="mt-10">
+        @if($errors->any())
+        <ul>
+          @foreach($errors->all() as $error)
+          <li class="text-red-500 italic">{{ $error }} </li>
+          @endforeach
+        </ul>
+        @endif
+      </div> -->
+
     </div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,12 @@ Route::get('/job/{id}', function ($id) {
 });
 
 Route::post('/jobs', function () {
+
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
