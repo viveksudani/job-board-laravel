1517b6db37616e4e9c0c99ab2419371d56952115
Vivek Sudani <viveksudani7@gmail.com>
2025-08-14 18:40:16 +0530

    use vite import instead of cdn links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
-
+    @vite('resources/css/app.css')    
 </head>
 <body class="h-full">
 
